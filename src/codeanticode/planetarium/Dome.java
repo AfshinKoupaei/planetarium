404ed5ba5319c7bb8d85fe9d4985e5f9c27afab6
codeanticode <andres.colubri@gmail.com>
2013-08-25 10:04:49 -0400

    first fully working version

--- src/codeanticode/planetarium/Dome.java
+++ src/codeanticode/planetarium/Dome.java
@@ -27,52 +27,241 @@
 
 package codeanticode.planetarium;
 
+import java.net.URL;
+import java.nio.IntBuffer;
 
-import processing.core.*;
+import processing.core.PApplet;
+import processing.core.PImage;
+import processing.core.PShape;
+import processing.opengl.PGL;
 import processing.opengl.PGraphics3D;
+import processing.opengl.PShader;
 
 /**
- * This is a template class and can be used to start a new processing library or tool.
- * Make sure you rename this class as well as the name of the example package 'template' 
- * to your own library or tool naming convention.
+ * Basic dome renderer.
  * 
- * @example Hello 
- * 
- * (the tag @example followed by the name of an example included in folder 'examples' will
- * automatically include the example in the javadoc.)
- *
  */
 
 public class Dome extends PGraphics3D {
   public final static String RENDERER = "codeanticode.planetarium.Dome";	
 	public final static String VERSION  = "##library.prettyVersion##";
 	
+  protected PShader cubemapShader;
+  protected PShape domeSphere;
+  protected PImage gridTex;
 
+  protected IntBuffer fbo;
+  protected IntBuffer rbo;
+  protected IntBuffer envMapTextureID;
+
+  protected boolean initialized = false;
+  protected int envMapSize = 1024;
+  
+  protected int currentFace;
+  
 	/**
-	 * a Constructor, usually called in the setup() method in your sketch to
-	 * initialize and start the library.
+	 * The default constructor.
 	 * 
-	 * @example Hello
-	 * @param theParent
 	 */
 	public Dome() {
 		super();
-		welcome();
+		welcome();		
 	}
 	
 	
-	private void welcome() {
-		System.out.println("##library.name## ##library.prettyVersion## by ##author##");
-	}
+  public void setParent(PApplet parent) {  // ignore
+    super.setParent(parent);
+    parent.registerMethod("pre", parent);
+  }
+  
 	
+  //////////////////////////////////////////////////////////////
 
+  // All projection methods are disabled
+
+	/*
+  @Override
+  public void ortho() {
+    showMethodWarning("ortho");
+  }
+
+
+  @Override
+  public void ortho(float left, float right,
+                    float bottom, float top) {
+    showMethodWarning("ortho");
+  }
+
+
+  @Override
+  public void ortho(float left, float right,
+                    float bottom, float top,
+                    float near, float far) {
+    showMethodWarning("ortho");
+  }
+
+
+  @Override
+  public void perspective() {
+    showMethodWarning("perspective");
+  }
+
+
+  @Override
+  public void perspective(float fov, float aspect, float zNear, float zFar) {
+    showMethodWarning("perspective");
+  }
+
+
+  @Override
+  public void frustum(float left, float right, float bottom, float top,
+                      float znear, float zfar) {
+    showMethodWarning("frustum");
+  }	
+*/
+  
+  //////////////////////////////////////////////////////////////
+
+  // Rendering
+  
+  
 	public void beginDraw() {
 	  super.beginDraw();
+	  
+	  if (0 < parent.frameCount) {	  
+	    if (!initialized) {
+	      initCubeMap();
+	    }
+	    
+	    beginPGL();
+	    
+	    // bind fbo
+	    pgl.bindFramebuffer(PGL.FRAMEBUFFER, fbo.get(0));   
+	    
+	    pgl.viewport(0, 0, envMapSize, envMapSize);    
+	    super.perspective(90.0f * DEG_TO_RAD, 1.0f, 1.0f, 1025.0f);     
+	    
+	    beginFaceDraw(PGL.TEXTURE_CUBE_MAP_POSITIVE_X);
+	  }
 	}
 
   public void endDraw() {
+    if (0 < parent.frameCount) {
+      endFaceDraw();
+
+      for (int face = PGL.TEXTURE_CUBE_MAP_NEGATIVE_X; 
+               face <= PGL.TEXTURE_CUBE_MAP_POSITIVE_Z; face++) {
+        beginFaceDraw(face);    
+        parent.draw();
+        endFaceDraw();      
+      }
+      
+      endPGL();
+      
+      drawDomeMaster(); 
+    }    
     super.endDraw();    
   }
 	
+  private void initCubeMap() {
+    gridTex = parent.loadImage("grid.png");
+    sphereDetail(50);
+    domeSphere = createShape(SPHERE, height/2.0f);
+    domeSphere.setTexture(gridTex);
+    domeSphere.rotateX(HALF_PI);
+    domeSphere.setStroke(false);
+    
+    PGL pgl = beginPGL();
+    
+    envMapTextureID = IntBuffer.allocate(1);
+    pgl.genTextures(1, envMapTextureID);
+    pgl.bindTexture(PGL.TEXTURE_CUBE_MAP, envMapTextureID.get(0));
+    pgl.texParameteri(PGL.TEXTURE_CUBE_MAP, PGL.TEXTURE_WRAP_S, 
+                      PGL.CLAMP_TO_EDGE);
+    pgl.texParameteri(PGL.TEXTURE_CUBE_MAP, PGL.TEXTURE_WRAP_T, 
+                      PGL.CLAMP_TO_EDGE);
+//    pgl.texParameteri(PGL.TEXTURE_CUBE_MAP, PGL.TEXTURE_WRAP_R, PGL.CLAMP_TO_EDGE);
+    pgl.texParameteri(PGL.TEXTURE_CUBE_MAP, PGL.TEXTURE_MIN_FILTER, 
+                      PGL.NEAREST);
+    pgl.texParameteri(PGL.TEXTURE_CUBE_MAP, PGL.TEXTURE_MAG_FILTER, 
+                      PGL.NEAREST);
+    for (int i = PGL.TEXTURE_CUBE_MAP_POSITIVE_X; i < 
+                 PGL.TEXTURE_CUBE_MAP_POSITIVE_X + 6; i++) {
+      pgl.texImage2D(i, 0, PGL.RGBA8, envMapSize, envMapSize, 0, 
+                     PGL.RGBA, PGL.UNSIGNED_BYTE, null);
+    }
+    
+    // Init fbo, rbo
+    fbo = IntBuffer.allocate(1);
+    rbo = IntBuffer.allocate(1);
+    pgl.genFramebuffers(1, fbo);
+    pgl.bindFramebuffer(PGL.FRAMEBUFFER, fbo.get(0));
+    pgl.framebufferTexture2D(PGL.FRAMEBUFFER, PGL.COLOR_ATTACHMENT0, 
+                             PGL.TEXTURE_CUBE_MAP_POSITIVE_X, 
+                             envMapTextureID.get(0), 0);
+
+    pgl.genRenderbuffers(1, rbo);
+    pgl.bindRenderbuffer(PGL.RENDERBUFFER, rbo.get(0));
+    pgl.renderbufferStorage(PGL.RENDERBUFFER, PGL.DEPTH_COMPONENT24, 
+                            envMapSize, envMapSize);
+    
+    // Attach depth buffer to FBO
+    pgl.framebufferRenderbuffer(PGL.FRAMEBUFFER, PGL.DEPTH_ATTACHMENT, 
+                                PGL.RENDERBUFFER, rbo.get(0));    
+
+    pgl.enable(PGL.TEXTURE_CUBE_MAP);
+    pgl.activeTexture(PGL.TEXTURE1);
+    pgl.bindTexture(PGL.TEXTURE_CUBE_MAP, envMapTextureID.get(0));     
+    
+    endPGL();
+    
+    // Load cubemap shader.
+    cubemapShader = parent.loadShader("cubemapfrag2.glsl", "cubemapvert2.glsl"); 
+    cubemapShader.set("EnvMap", 1);
+    
+    initialized = true;
+  }
+  
+  private void beginFaceDraw(int face) {
+    currentFace = face; 
+    System.out.println("Drawing face " + face);
+    
+    resetMatrix();
+    
+    if (currentFace == PGL.TEXTURE_CUBE_MAP_POSITIVE_X) {
+      camera(0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, -1.0f, 0.0f);
+    } else if (currentFace == PGL.TEXTURE_CUBE_MAP_NEGATIVE_X) {
+      camera(0.0f, 0.0f, 0.0f, -1.0f, 0.0f, 0.0f, 0.0f, -1.0f, 0.0f);
+    } else if (currentFace == PGL.TEXTURE_CUBE_MAP_POSITIVE_Y) {
+      camera(0.0f, 0.0f, 0.0f, 0.0f, -1.0f, 0.0f, 0.0f, 0.0f, -1.0f);  
+    } else if (currentFace == PGL.TEXTURE_CUBE_MAP_NEGATIVE_Y) {
+      camera(0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 1.0f);
+    } else if (currentFace == PGL.TEXTURE_CUBE_MAP_POSITIVE_Z) {
+      camera(0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, -1.0f, 0.0f);    
+    } else if (currentFace == PGL.TEXTURE_CUBE_MAP_NEGATIVE_Z) {
+      camera(0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f);
+    }
+    
+    pgl.framebufferTexture2D(PGL.FRAMEBUFFER, PGL.COLOR_ATTACHMENT0, 
+                             currentFace, envMapTextureID.get(0), 0);
+  }
+  
+  private void endFaceDraw() {
+    flush(); // Make sure that the geometry in the scene is pushed to the GPU
+    pgl.framebufferTexture2D(PGL.FRAMEBUFFER, PGL.COLOR_ATTACHMENT0, 
+                             currentFace, 0, 0);
+  }
+  
+  private void drawDomeMaster() {
+    ortho();
+    resetMatrix();
+    shader(cubemapShader);
+    shape(domeSphere);
+    resetShader();
+  }
+  
+  private void welcome() {
+    System.out.println("##library.name## ##library.prettyVersion## by ##author##");
+  }  
 }
 
